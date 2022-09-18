a1a020f91a6ea1b3dcbe02c71624f0efde4c10e0
nvcuong1 <vietcuong.uet@gmail.com>
2022-09-18 23:52:54 +0700

    Tai cau truc demo test

---
+++ tests/unit/component/example.spec.js
@@ -0,0 +1,24 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+import UnitTestHelpers from '../common/UnitTestHelpers'
+
+// Nhóm các test case
+describe('HelloWorld.vue', () => {
+
+  // Mô tả test case và chi tiết ntn
+  it('renders props.msg when passed', () => {
+    // set up
+    // const msg = 'new message'
+    // // act
+    // const wrapper = shallowMount(HelloWorld, {
+    //   propsData: { msg }
+    // })
+    // let h = new UnitTestHelpers(wrapper, expect)
+    // // assert 
+    // h.textContainInDOM('new', ".hello");
+
+    expect(("abc")).toContain("a");
+  })
+
+
+})
