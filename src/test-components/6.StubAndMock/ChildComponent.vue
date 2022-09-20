349384f6605cb4bb3476065b0808e2d5f6999dfd
CUONG1 NGUYEN VIET <nvcuong1@misa-group.vn>
2022-09-20 14:17:50 +0700

    them doc ve stub and mock

---
+++ src/test-components/6.StubAndMock/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="full-name-child">
+        {{fullName}}
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "ChildComponent",
+    data() {
+        return {
+            fullName: "child component"
+        }
+    },
+    async mounted() {
+        // some fake API
+        const res = await axios.get('/someAPI');
+        
+        this.afterGetAPI(res);
+    },
+    methods:{
+        afterGetAPI(res){
+            this.fullName = res.data;
+        }
+    }
+}
+</script>
