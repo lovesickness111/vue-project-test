208529fd8a11e80c57d50a68df4c5d1da35bf746
nqdat <datnq1210@gmail.com>
2022-09-22 22:42:58 +0700

    update unit snap test

--- tests/unit/common/7.SnapshotTest/Math.spec.js
+++ tests/unit/common/7.SnapshotTest/Math.spec.js
@@ -1,5 +1,7 @@
 import { mount } from "@vue/test-utils";
-import App from '@/components/MathApp.vue';
+import App from '@/test-components/7.SnapShotTest/MathApp.vue';
+
+jest.spyOn(axios, 'get').mockResolvedValue(function () { return true; });   
 
 describe("App", () => {
     // Inspect the raw component options
@@ -9,43 +11,6 @@ describe("App", () => {
 });
 
 describe("Mounted App", () => {
-    const wrapper = mount(App);
-
-    test("is a Vue instance", () => {
-        expect(wrapper.isVueInstance()).toBeTruthy();
-    });
-
-    it("renders the correct markup", () => {
-        expect(wrapper.html()).toContain(
-            "<p>What is the sum of the two numbers?</p>"
-        );
-    });
-
-    // it's also easy to check for the existence of elements
-    it("has a buttons", () => {
-        expect(wrapper.contains("button")).toBe(true);
-    });
-
-    it("renders correctly with different data", async () => {
-        wrapper.setData({ x1: 5, x2: 10 });
-        await wrapper.vm.$nextTick();
-        expect(wrapper.text()).toContain("10");
-    });
-
-    it("button click without correct sum", () => {
-        expect(wrapper.vm.message).toBe("");
-        const button = wrapper.find("button");
-        button.trigger("click");
-        expect(wrapper.vm.message).toBe("TRY AGAIN");
-    });
-
-    it("button click with correct sum", () => {
-        wrapper.setData({ guess: "15" });
-        const button = wrapper.find("button");
-        button.trigger("click");
-        expect(wrapper.vm.message).toBe("SUCCESS!");
-    });
-
     it('renders correctly', () => {
         const wrapper = mount(App, {
             data() {
@@ -61,6 +26,6 @@ describe("Mounted App", () => {
     it('test message', () => {
         const wrapper = mount(App);
         wrapper.vm.check();
-        expect(wrapper.vm.nvcuong1.name).toBe('a');
+        expect(wrapper.vm.nvcuong1.name).toBe('nvcuong1');
     });
 });
