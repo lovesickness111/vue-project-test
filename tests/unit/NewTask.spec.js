c3f9b474ea87fbf22a49e93acbf983de75aee782
CUONG1 NGUYEN VIET <nvcuong1@misa-group.vn>
2022-09-23 13:46:35 +0700

    sua route loi + demo mock function

--- tests/unit/NewTask.spec.js
+++ tests/unit/NewTask.spec.js
@@ -1,26 +1,48 @@
-import { mount } from '@vue/test-utils'
-import NewTask from '@/components/NewTask'
+import { mount } from "@vue/test-utils";
+import NewTask from "@/components/NewTask";
+import { nextTick } from "vue";
 
-test('emits an event with count when clicked', () => {
-  const wrapper = mount(NewTask)
+describe("Test them cong viec", () => {
+  test("test them va hien thi cong viec", () => {
+    const wrapper = mount(NewTask);
+    // 1 nhập teen công việc = Arrange
 
-  //Set tên task mới là : write unit test
-  const input = wrapper.find('input');
-  input.setValue("write unit test");
-  wrapper.find('button').trigger('click'); // lần đầu emit ra giá trị new task
-  wrapper.find('button').trigger('click'); // Lần 2 để test xem đã clear text input chưa
+    // 2 bấm click thêm = Action
 
-  //Check xem có event addNewTask không 
-  expect(wrapper.emitted()).toHaveProperty('addNewTask');
-  
-  const addNewTask = wrapper.emitted('addNewTask');
+    // 3 kiểm tra = Assert
 
-  //Check xem có emit ra đúng task name
-  expect(wrapper.emitted()).toHaveProperty('addNewTask');
-  expect(addNewTask[0]).toEqual(['write unit test'])
+    // console.log(wrapper.vm);
 
-  //Check xem sau khi thêm mới đã clear text input chưa
-  expect(addNewTask[1]).toEqual([''])
+    //Set tên task mới là : write unit test
+    const input = wrapper.find("input");
+    input.setValue("write unit test");
+    wrapper.find("button").trigger("click"); // lần đầu emit ra giá trị new task
 
+    wrapper.find("button").trigger("click"); // Lần 2 để test xem đã clear text input chưa
 
-})
+    //Check xem có event addNewTask không
+    expect(wrapper.emitted()).toHaveProperty("addNewTask");
+
+    const addNewTask = wrapper.emitted("addNewTask");
+
+    //Check xem có emit ra đúng task name
+    expect(wrapper.emitted()).toHaveProperty("addNewTask");
+    expect(addNewTask[0]).toEqual(["write unit test"]);
+
+    //Check xem sau khi thêm mới đã clear text input chưa
+    expect(addNewTask[1]).toEqual([""]);
+  });
+
+  test("test khi click button thi ham addNewTask co duoc goi khong", async () => {
+    const wrapper = mount(NewTask);
+    const spy = jest.spyOn(wrapper.vm, "addNewTask");
+
+    //Set tên task mới là : write unit test
+    const input = wrapper.find("input");
+    await input.setValue("write unit test nvcuong1");
+    await wrapper.find("button").trigger("click"); // lần đầu emit ra giá trị new task
+
+ 
+    expect(wrapper.vm.addNewTask).toBeCalled();
+  });
+});
